0c02ae78162b672f7d0165385093ff5a1fc6325b
Isabella White <isabella.white8248@gmail.com>
2026-04-16 09:21:46 -0500

    Initial Xcode project

---
+++ Mangez/Mangez/MangezApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangezApp.swift
+//  Mangez
+//
+//  Created by Isabella White on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MangezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mangez/MangezTests/MangezTests.swift
@@ -0,0 +1,17 @@
+//
+//  MangezTests.swift
+//  MangezTests
+//
+//  Created by Isabella White on 4/16/26.
+//
+
+import Testing
+@testable import Mangez
+
+struct MangezTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
